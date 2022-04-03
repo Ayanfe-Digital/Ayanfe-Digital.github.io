3763df9bfa1a9bbcf36e29b08131a5827d2bbd37
Ayanfe-Digital <ayanfe.s.art@outlook.com>
2022-04-03 11:26:13 +0100

    change

--- About/My Coily Story/main.js
+++ About/My Coily Story/main.js
@@ -10,7 +10,7 @@ closeMenu.addEventListener('click',close);
 
 function show(){
     mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    mainMenu.style.top = '-100%';
 }
 function close(){
     mainMenu.style.top = '-200%';
